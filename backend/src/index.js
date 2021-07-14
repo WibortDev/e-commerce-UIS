3141b9c0de95daa9f83a5859355824652e268b44
Luis Osorio <luisfosgdev@gmail.com>
2021-07-14 16:01:04 -0500

    ADD: Config eslint, database connection..

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,8 @@
+import './config';
 import app from './app';
 
+import './database';
+
 app.listen( app.get( 'port' ), () => {
-	console.log( `Servidor listo en el puerto ${ app.get( 'port' ) }` );
+	console.log( `\n Servidor listo en el puerto ${ app.get( 'port' ) }` );
 } );
