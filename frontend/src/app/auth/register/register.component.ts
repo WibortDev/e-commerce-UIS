b4607e51f9d11797d053b0c6312301750f3f017b
Luis Osorio <luisfosg@gmail.com>
2021-07-29 19:25:23 -0500

    ADD: basic form login/register

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserModel } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: UserModel = {
+    name: '',
+    email: '',
+    password: ''
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSignUp(form: NgForm): void {
+    console.log(form);
+  }
 }
